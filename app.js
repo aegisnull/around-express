31db13b79354350e71ef0b9fca4a2630d6a8ad75
Luis Tellez <luis@luistellez.com>
2022-11-01 01:17:25 -0700

    refactor: move routing for users to external module

--- app.js
+++ app.js
@@ -3,29 +3,14 @@ const express = require("express");
 const { PORT = 3000 } = process.env;
 const app = express();
 
-//const userRouter = require("./routes/users");
+const usersRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
 
 app.use("/", cardRouter);
+app.use("/", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-});
-
-// set route for http://localhost:3000/users
-app.get("/users", (req, res) => {
-  res.json(require("./data/users.json"));
-});
-
-// set route for http://localhost:3000/users/8340d0ec33270a25f2413b69 and show only that _id if id does not match, API should send JSON: { "message": "ID de usuario no encontrado" }
-app.get("/users/:_id", (req, res) => {
-  const { _id } = req.params;
-  const user = require("./data/users.json").find((user) => user._id === _id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: "ID de usuario no encontrado" });
-  }
 });
 
 // set route for Non-existent address or localhost:3000
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
-const cardsRouter = require("express").Router();
-const fs = require("fs").promises;
+const usersRouter = require("express").Router();
+const fsPromises = require("fs").promises;
+const { resolvePtr } = require("dns");
 const path = require("path");
 
-const cardsPath = path.join(__dirname, "../data/cards.json");
+const usersPath = path.join(__dirname, "../data/users.json");
 
-cardsRouter.get("/cards", (req, res) => {
-  fs.readFile(cardsPath, { encoding: "utf8" })
+usersRouter.get("/users", (req, res) => {
+  fsPromises
+    .readFile(usersPath, { encoding: "utf8" })
     .then((data) => {
       res.json(JSON.parse(data));
     })
@@ -13,3 +15,15 @@ cardsRouter.get("/cards", (req, res) => {
       console.log(err);
     });
 });
+
+usersRouter.get("/users/:_id", (req, res) => {
+  const { _id } = req.params;
+  const user = require("../data/users.json").find((user) => user._id === _id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "ID de usuario no encontrado" });
+  }
+});
+
+module.exports = usersRouter;
